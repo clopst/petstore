719c5650b521fe535977d3e0dadf081c8fabd365
Dimas Nurfauzi <dimasantafie@gmail.com>
2022-09-08 21:32:35 +0700

    feat: create postgres database connection

---
+++ internal/database/postgres.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "petstores"
+)
+
+var DBClient *sql.DB
+
+func init() {
+	connectionString := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	var err error
+	DBClient, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database.")
+}
